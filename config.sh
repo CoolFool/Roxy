8140a63fcdba15e441ac576e71472d383f8a6e4b
coolfool <coolfool149@gmail.com>
2022-04-11 00:16:17 +0530

    keep alive msg

--- config.sh
+++ config.sh
@@ -4,7 +4,7 @@ set -o pipefail
 
 if [[ "${KEEP_DYNO_ALIVE,,}" == "true" ]];then
     nohup python keep_alive.py >/dev/null 2>&1 &
-    echo "Success"
+    echo "Heroku FREE dyno will be kept alive"
 else
     echo "Heroku FREE dyno will auto-sleep after 20 minutes"
 fi
